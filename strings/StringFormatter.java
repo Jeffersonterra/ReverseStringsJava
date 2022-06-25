3820fb5179321ceb470a3a9c8386194b33e1545a
Jeffersonterra <104329745+Jeffersonterra@users.noreply.github.com>
2022-06-25 17:10:51 -0300

    Add files via upload

---
+++ strings/StringFormatter.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringFormatter {
+    public static String reverseString(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+}
---
+++ strings/TestStringFormatter.java
@@ -0,0 +1,8 @@
+package strings;
+
+public class TestStringFormatter {
+    public static void main(String[] args) {
+        System.out.println(StringFormatter.reverseString("I am jefferson"));
+        System.out.println(StringFormatter.reverseString("Hi jefferson, hi lorena, my name is larissa"));
+    }
+}
